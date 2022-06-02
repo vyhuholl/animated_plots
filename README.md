7dadcafd739356241e7ccc8b70a357c1c4e567f4
Olga Pichuzhkina <vyhuholl@protonmail.com>
2022-06-02 20:50:34 +0300

    added line plot

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # animated_plots
-A small exercise – plots animated with ImageIO library.
+A small exercise – plots animated with ImageIO library.<br>
+![Animated line plot](line.gif)
---
+++ generate_line_plot.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main(size: int, filename: str, frames_dir: str):
+    '''
+    Generates a GIF of a line plot of a NumPy array.
+
+    Parameters:
+        size: int
+        filename: str
+        frames_dir: str
+
+    Returns:
+        None
+    '''
+    y = np.random.randint(-5, 5, size)
+
+    for i in range(2, size + 1):
+        plt.rcParams['figure.figsize'] = (16, 10)
+        plt.plot(y[0: i])
+        plt.ylim(-10, 10)
+        plt.savefig(os.path.join(frames_dir, f'line-{i}.png'))
+        plt.close()
+
+    with imageio.get_writer(filename, mode='i') as writer:
+        for i in range(2, size + 1):
+            image = imageio.v2.imread(
+                os.path.join(frames_dir, f'line-{i}.png')
+                )
+            writer.append_data(image)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='generate_line_plot',
+        description='Generates a GIF of a line plot of a NumPy array.'
+        )
+
+    parser.add_argument('size', type=int, help='size of the array to plot')
+
+    parser.add_argument(
+        '-o', '--output', default='line.gif', help='name of the output file'
+        )
+
+    parser.add_argument(
+        '-f', '--frames', default='frames/line', help='path to the frames dir'
+        )
+
+    args = parser.parse_args()
+    main(args.size, args.output, args.frames)
